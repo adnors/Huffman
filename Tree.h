5fa7a0248b0c1f07f1e8e99c6e08698f1c689702
epitox <daniel.haensler@gmx.de>
2016-03-08 11:49:26 +0100

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -13,23 +13,23 @@ using namespace std;
 
 class Tree {
 private:
-    Tree *root;
-    Tree *rTree;
-    Tree *lTree;
-    int value;
-    char character;
+    Tree *root; //Wurzel des Baums
+    Tree *rTree; //Rechter Teilbaum
+    Tree *lTree; //Linker Teilbaum
+    int value; //Wert des Knotens
+    char character; //Zeichen des Knotens
 
 public:
-    Tree(Tree *root, Tree *rightTree, Tree *leftTree, int v, char c);
-    Tree *getRoot();
-    Tree *getRightTree();
-    Tree *getLeftTree();
-    int getValue();
-    char getCharacter();
-    void setRoot(Tree *);
-    string toString();
+    Tree(Tree *root, Tree *rightTree, Tree *leftTree, int v, char c); //Konstruktor mit Wurzel, Teilbäumen, Wert und Zeichen
+    Tree *getRoot(); //Gibt die Wurzel zurück
+    Tree *getRightTree(); //Gibt den rechten Teilbaum zurück
+    Tree *getLeftTree(); //Gibt den linken Teilbaum zurück
+    int getValue(); //Gibt den Wert des Knotens zurück
+    char getCharacter(); //Gibt das Zeichen des Knotens zurück
+    void setRoot(Tree *); //Definiert die Baumwurzel
+    string toString(); //Ausgabe der Baumparamter als String
 
-    bool isLeaf();
+    bool isLeaf(); //True, wenn Knoten ein Blatt ist
 };
 
 
